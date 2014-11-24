bc4cf592ecfdedab50afe76978f7329fad8fc81e
marcwebbie <marcwebbie@gmail.com>
2014-11-24 04:17:43 -0300

    Updating conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Marcwebbie'
 SITENAME = u'Quilombist'
-SITEURL = ''
+SITEURL = '//localhost:8000'
+SITESUBTITLE = 'Technology and pan-africanism together'
+TAGLINE = SITESUBTITLE
 
 PATH = 'content'
 
@@ -27,7 +29,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 
 # Social widget
 SOCIAL = (('twitter', 'https://twitter.com/marcwebbie'),
-          ('github.com', '//github.com/marcwebbie'),)
+          ('github', '//github.com/marcwebbie'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -38,15 +40,26 @@ OUTPUT_PATH = "./quilombist.github.io"
 
 TYPOGRIFY = True
 
+STATIC_PATHS = ['images']
+
 # Theme
-# THEME = "./themes/pelican-bootstrap3"
-# THEME = "./themes/SoMa"
-# THEME = "./themes/SoMa2"
-# THEME = "./themes/Cahier"
-THEME = "./themes/Just-Read"
+THEME = "./themes/PureMod"
+# THEME = "./local_themes/pelican-bootstrap3"
+# THEME = "./local_themes/SoMa"
+# THEME = "./local_themes/SoMa2"
+# THEME = "./local_themes/Cahier"
+# THEME = "./local_themes/Just-Read"
+# THEME = "./local_themes/pure-single"
+# THEME = "./local_themes/fresh-mod"
+# THEME = "./local_themes/octopress"
+# THEME = "./local_themes/simplegrey"
+# THEME = "./local_themes/pure"
+# THEME = "./local_themes/cahier"
+# THEME = "./local_themes/foundation"
+
 
 # Urls
-ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
+# ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+# ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+# PAGE_URL = 'pages/{slug}/'
+# PAGE_SAVE_AS = 'pages/{slug}/index.html'
